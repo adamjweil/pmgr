8b655d8d81bfee1e644025b32cb0a039a162394d
Adam Weil <adamweil@Adams-MacBook-Pro.local>
2020-11-17 17:29:30 -0500

    initial commit

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    role: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'roles',
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('user', UserSchema);
